d78dd15cf3f7221e198f30e3fb758cccdf12bef8
HashGen <abechuphojaoff@gmail.com>
2025-12-20 18:05:12 +0530

    Update links.js

--- links.js
+++ links.js
@@ -2,5 +2,6 @@ const AFFILIATE_TAG = "royalkrrish00-21"; // Apna Tag Yahan Daalo
 
 const products = [
     "https://www.amazon.in/dp/B0DTTZ86TV",
+    "https://amzn.to/491Lwbp",
     // Aur links yahan comma (,) laga ke add karte jao
 ];
